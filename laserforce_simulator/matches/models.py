c969349c518fec0eeaae277cf6dab84b0d02b7c3
Andrew Galkowski <atgalkowski@gmail.com>
2025-10-16 18:25:34 -0700

    changed str return for playerRoundState

--- laserforce_simulator/matches/models.py
+++ laserforce_simulator/matches/models.py
@@ -310,7 +310,7 @@ class PlayerRoundState(models.Model):
     was_eliminated = models.BooleanField(default=False)  # Ran out of lives
 
     def __str__(self):
-        return f"{self.player.name} - {self.game_round}"
+        return f"{self.player.name}:{self.player.id}:{self.team_color}:{self.role} - {self.game_round}"
 
     @property
     def max_shields(self):
